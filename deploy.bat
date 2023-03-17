bc80d01e67c028f97307f13d5d688b8849ba2bd6
nguyenvancongdev <congthang.nuce@gmail.com>
2023-03-17 21:53:01 +0700

    deploy

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ echo qtdlt.congnguyen.dev > CNAME
 git init
 git add -A
 git commit -m "hello"
-git push -f https://github.com/nguyenvancongdev/qtdlt.git main:gh-pages
+git push -f https://github.com/nguyenvancongdev/qtdlt.git master:gh-pages
 pause
 echo .
