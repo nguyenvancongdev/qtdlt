f6d94d41af426f388d1ea30c548b216d6c16b13c
nguyenvancongdev <congthang.nuce@gmail.com>
2023-03-17 21:40:07 +0700

    deploy

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 cd dist
-echo vuejs-demo.congnguyen.dev > CNAME
+echo qtdlt.congnguyen.dev > CNAME
 git init
 git add -A
 git commit -m "hello"
-git push -f https://github.com/nguyenvancongdev/vuejs-demo.git master:gh-pages
+git push -f https://github.com/nguyenvancongdev/qtdlt.git main:gh-pages
 pause
 echo .
