d3898fb03a9dabcc30baf6c938941a19734db7d9
Nguyen Van Cong <congthang.nuce@gmail.com>
2023-04-05 03:28:12 +0000

    Update index.js

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,3 +1,5 @@
-module.exports.getAll = () => {
-    return 'hello'
+connect = (row) => (db.collection(row))
+module.exports.getAll = ({row, id, data}) => {
+    let connect = connect(row)
+    let data = await connect.
 }
